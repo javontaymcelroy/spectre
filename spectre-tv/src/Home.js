c336fdab96c0f6984a97057e6067d82866ee04bd
Javontay McElroy <dinomightnetwork@gmail.com>
2019-03-02 18:31:22 -0600

    Project set up.

---
+++ spectre-tv/src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return <div className='Home' />;
+  }
+}
+
+export default Home;
---
+++ spectre-tv/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import './index.css';
+import Home from './Home';
+
+ReactDOM.render(
+  <Router>
+    {' '}
+    <Home />{' '}
+  </Router>,
+  document.getElementById('root')
+);
