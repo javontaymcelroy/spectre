10feea82376e4ea670622f091ab6046a1d774801
Javontay McElroy <dinomightnetwork@gmail.com>
2019-08-08 11:44:53 -0500

    Spectre is getting an upgrade!

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Home</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
