275dad7bee46692cb316593be961a9b77cee00f8
Javontay McElroy <dinomightnetwork@gmail.com>
2019-08-11 01:33:06 -0500

    Fixed styling.

--- src/components/PopularPosters.js
+++ src/components/PopularPosters.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Posters.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Posters.css";
 
-const PopularPosters = React.forwardRef(
-  ({ popular, addDefaultSrc, onWheel }, popref) => (
-    <div className='poster-container scroller' ref={popref} onWheel={onWheel}>
-      {popular.map(popular => (
-        <div key={popular.id}>
-          <NavLink to={`/TvShow/${popular.id}/1`} className='title-links'>
-            <img
-              src={`http://image.tmdb.org/t/p/w500${popular.poster_path}`}
-              alt={popular.name}
-              className='posters'
-              onError={addDefaultSrc}
-            />
-            <h3 className='poster-title'> {popular.name} </h3>
-          </NavLink>
-        </div>
-      ))}
-    </div>
-  )
+const PopularPosters = ({ popular, addDefaultSrc }) => (
+  <div className="poster-container scroller">
+    {popular.slice(1, 8).map(popular => (
+      <div key={popular.id}>
+        <NavLink to={`/TvShow/${popular.id}/1`} className="title-links">
+          <img
+            src={`http://image.tmdb.org/t/p/w500${popular.poster_path}`}
+            alt={popular.name}
+            className="posters"
+            onError={addDefaultSrc}
+          />
+          <h3 className="poster-title"> {popular.name} </h3>
+        </NavLink>
+      </div>
+    ))}
+  </div>
 );
 
 export default PopularPosters;
