21e7bdd1c67bd5e08f3e6340df5cb2c8cea3aace
Javontay McElroy <dinomightnetwork@gmail.com>
2019-08-11 01:53:47 -0500

    Updated styles, packages, scrolling issues.

--- src/components/PopularPosters.js
+++ src/components/PopularPosters.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import HorizontalScroll from "react-scroll-horizontal";
 import "./Posters.css";
 
 const PopularPosters = ({ popular, addDefaultSrc }) => (
   <div className="poster-container scroller">
-    {popular.slice(1, 8).map(popular => (
-      <div key={popular.id}>
-        <NavLink to={`/TvShow/${popular.id}/1`} className="title-links">
-          <img
-            src={`http://image.tmdb.org/t/p/w500${popular.poster_path}`}
-            alt={popular.name}
-            className="posters"
-            onError={addDefaultSrc}
-          />
-          <h3 className="poster-title"> {popular.name} </h3>
-        </NavLink>
-      </div>
-    ))}
+    <HorizontalScroll pageLock={true}>
+      {popular.slice(1, 8).map(popular => (
+        <div key={popular.id}>
+          <NavLink to={`/TvShow/${popular.id}/1`} className="title-links">
+            <img
+              src={`http://image.tmdb.org/t/p/w500${popular.poster_path}`}
+              alt={popular.name}
+              className="posters"
+              onError={addDefaultSrc}
+            />
+            <h3 className="poster-title"> {popular.name} </h3>
+          </NavLink>
+        </div>
+      ))}
+    </HorizontalScroll>
   </div>
 );
 
--- src/components/TopRatedPosters.js
+++ src/components/TopRatedPosters.js
@@ -1,29 +1,39 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Posters.css';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import HorizontalScroll from "react-scroll-horizontal";
 
-const TopRatedPosters = React.forwardRef(
-  ({ rated, addDefaultSrc, onWheel }, ref) => (
-    <div
-      className='poster-container-nohero scroller'
-      ref={ref}
-      onWheel={onWheel}
-    >
-      {rated.map(rated => (
-        <div key={rated.id}>
-          <NavLink to={`/TvShow/${rated.id}`} className='title-links'>
-            <img
-              src={`http://image.tmdb.org/t/p/w500${rated.poster_path}`}
-              alt={rated.name}
-              className='posters'
-              onError={addDefaultSrc}
-            />
-            <h3 className='poster-title'> {rated.name} </h3>
-          </NavLink>
+import "./Posters.css";
+
+class TopRatedPosters extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lockScroll: false
+    };
+  }
+  render() {
+    const rated = this.props.rated;
+    const addDefaultSrc = this.props.addDefaultSrc;
+    return (
+      <HorizontalScroll pageLock={true} reverseScroll={true}>
+        <div className="poster-container-nohero scroller">
+          {rated.map(rated => (
+            <div key={rated.id}>
+              <NavLink to={`/TvShow/${rated.id}`} className="title-links">
+                <img
+                  src={`http://image.tmdb.org/t/p/w500${rated.poster_path}`}
+                  alt={rated.name}
+                  className="posters"
+                  onError={addDefaultSrc}
+                />
+                <h3 className="poster-title"> {rated.name} </h3>
+              </NavLink>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  )
-);
+      </HorizontalScroll>
+    );
+  }
+}
 
 export default TopRatedPosters;
